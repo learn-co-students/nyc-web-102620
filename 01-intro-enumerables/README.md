f09640a9828cc9b7c9316fc1b9c4829ae85c8d50
Caryn McCarthy <cmccarthy15@gmail.com>
2020-10-26 09:19:28 -0400

    intro enumerables starter

---
+++ 01-intro-enumerables/README.md
@@ -0,0 +1,28 @@
+# Ruby Enumerables & Pry
+
+## Prerequisites
+
+- DataTypes: 
+- Conditionals 
+- Loops 
+- Arrays vs Hashes
+- Accessing Arrays & Accessing Hashes
+- Iterating through Arrays and Hashes using `.each`
+
+## Agenda
+
+- Mindset:
+- Enumerables:
+    - .each
+    - .map && .collect 
+    - .filter && .select 
+    - .find
+- Pry
+- Show where this code lives on github and how to access it! 
+
+
+## Icebox
+
+
+
+
---
+++ 01-intro-enumerables/run.rb
@@ -0,0 +1,14 @@
+# 1. I am building a Pets application. I need to a way to hold all the information (name, type) about a SPECIFIC pet. Which datatype should I use?
+# 2. Now, I need a way to hold a list of all of the pets in my app. Which datatype should I use? 
+# 3. I want a list of only the names of each pet. How do I do that? What datatype should be returned?
+# 4. I want to be to find all pets that are dogs. What iterator should I use? What datatype should be returned?
+# 5. I want to be able to find a single dog with a specific name. How do I do that? What datatype should be returned?
+
+# 1. show a list of all the names of all my pets
+# 2. show me a list of just the pets that are... cats?
+# 3. let me search for pet by name
+
+
+def run
+    puts "Application is running"
+end
