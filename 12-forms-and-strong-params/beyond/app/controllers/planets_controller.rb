dde2ec7e8d804af9ce57e656c3b1c878802e0595
Steven Doran <steven.doran@flatironschool.com>
2020-11-18 16:54:48 -0500

    post lecture

--- 12-forms-and-strong-params/beyond/app/controllers/planets_controller.rb
+++ 12-forms-and-strong-params/beyond/app/controllers/planets_controller.rb
@@ -1,16 +1,31 @@
 class PlanetsController < ApplicationController
   def index 
-    byebug
     @planets = Planet.all
     # render :index
   end 
 
+  def new
+    @planet = Planet.new
+    
+    # render :new
+  end
 
+  def create
+    @planet = Planet.create!(planet_params)
+
+    redirect_to planet_path(@planet)
+  end
+  
   def show 
     @planet = Planet.find(params[:id])
     
     # render :show
   end 
 
+  private
+
+  def planet_params
+    params.require(:planet).permit(:name, :size)
+  end
 
 end
--- 12-forms-and-strong-params/beyond/config/routes.rb
+++ 12-forms-and-strong-params/beyond/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/planets', to: 'planets#index'
-  get '/planets/:id', to: 'planets#show'
+  # get '/planets', to: 'planets#index', as: 'planets'
+  # get '/planets/:id', to: 'planets#show', as: 'planet'
 
+  resources :planets, only: [:index, :show, :new, :create]
 end
