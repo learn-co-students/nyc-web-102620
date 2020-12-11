154a3c9932800b6b7ad05f5958550e21342ef48a
Eric Kim <hyeokjungkim@gmail.com>
2020-12-11 13:01:06 -0500

    adds lecture

---
+++ 22-events/index.js
@@ -0,0 +1,23 @@
+let postsArray = [
+    {
+        meme: 'pics/dino.jpeg',
+        description: 'Ahhh!!!'
+
+    },
+    {
+        meme: 'pics/elvish.jpeg',
+        description: 'Hmm 🤔'
+
+    },
+    {
+        meme: 'pics/space.jpeg',
+        description: 'Have standards people, standards!!!'
+
+    },
+    {
+        meme: 'pics/restaurant.jpg',
+        description: 'Let\'s wine & dine!'
+
+    }
+]
+
