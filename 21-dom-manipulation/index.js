a727e58c89fbbb5ca35227de1baa6e65e03bf287
Michelle Rios <brendamichelle995@gmail.com>
2020-12-10 23:51:55 -0500

    before lecture

---
+++ 21-dom-manipulation/index.js
@@ -0,0 +1,79 @@
+console.log("I should appear on the console!")
+
+/*********** CRUD with the DOM ***********/
+// https://developer.mozilla.org/en-US/
+// https://www.w3schools.com/cssref/css_selectors.asp 
+
+// Read
+
+// Update
+
+// Delete
+
+// Create
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TODO Create post for each object in this array
+
+const listingsArray = [
+    {
+        imageLocation: "pics/bugfix.jpeg",
+        title: "Fixing Bugs",
+        price: "$29.99",
+        description: "We all come to know this moment",
+        rating: 3.01
+    },
+    {
+        imageLocation: "pics/charAndString.jpeg",
+        title: "Kindness",
+        price: "$19.99",
+        description: "A reminder that we can all be kind",
+        rating: 5.0
+    },
+    {
+        imageLocation: "pics/dino.jpeg",
+        title: "Dinosaur Evolution",
+        price: "$79.99",
+        description: "Beautiful piece showing evolution",
+        rating: 4.72
+    },
+    {
+        imageLocation: "pics/documentation.png",
+        title: "Updating Docs",
+        price: "$30.00",
+        description: "Never forget to update your documentation",
+        rating: 3.51
+    },
+    {
+        imageLocation: "pics/elvish.jpeg",
+        title: "What's this? LOTR",
+        price: "$99.99",
+        description: "For that LOTR & programming lover in your life",
+        rating: 4.51
+    }
+]
