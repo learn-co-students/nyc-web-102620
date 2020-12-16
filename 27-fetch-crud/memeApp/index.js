4a10b0f2c94e0ab0e425114fa7d76a5d07fe334c
ihollander <ianholla@gmail.com>
2020-12-16 14:22:38 -0500

    After fetch crud

--- 27-fetch-crud/memeApp/index.js
+++ 27-fetch-crud/memeApp/index.js
@@ -26,10 +26,13 @@ function renderOneMeme(postObject) {
 
   deleteButton.classList.add("delete-button");
   deleteButton.textContent = "X";
+  deleteButton.dataset.id = postObject.id;
 
   likeButton.classList.add("likes-button");
   likeButton.dataset.buttonType = "upvote";
   likeButton.textContent = "👍";
+  likeButton.dataset.id = postObject.id;
+
   numOfLikesPtag.textContent = postObject.likes;
   numOfLikesPtag.classList.add("likes-count");
 
@@ -47,33 +50,100 @@ function renderAllListings(memePostsArray) {
 }
 
 /******************* Event Listeners  ******************/
-
+// Event Delegation
 listings.addEventListener("click", function (event) {
   if (event.target.matches(".delete-button")) {
     // Delete Meme
+    // when the delete button is clicked
+
+    // make a DELETE /posts/10 fetch request
+    const id = event.target.dataset.id;
+    fetch(`http://localhost:3000/posts/${id}`, {
+      method: "DELETE",
+    });
+    // .then((res) => res.json())
+    // .then((data) => {
+    //   console.log(data);
+    //   // pessimistic rendering (not lying to your users)
+    //   const postDiv = event.target.closest("div.post");
+    //   postDiv.remove();
+    // });
+
+    // optimistic rendering (maybe lying to your users)
     const postDiv = event.target.closest("div.post");
     postDiv.remove();
   } else if (event.target.dataset.buttonType === "upvote") {
     // Update Meme
+    // when the update button is clicked
+
+    // make PATCH /posts/100 fetch request
     const postDiv = event.target.closest("div.post");
     const likesPtag = postDiv.querySelector(".likes-count");
     const numOfLikes = parseInt(likesPtag.textContent) + 1;
-    likesPtag.textContent = numOfLikes;
+
+    const id = event.target.dataset.id;
+
+    fetch(`http://localhost:3000/posts/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        likes: numOfLikes,
+      }),
+    })
+      .then((res) => res.json())
+      .then((updatedPost) => {
+        console.log(updatedPost);
+        // pessimists!
+        likesPtag.textContent = updatedPost.likes;
+      });
+
+    // and update the likes on the DOM
+    // optimists!
+    // likesPtag.textContent = numOfLikes;
   }
 });
 
+// When the form submit event happens...
 newMemeForm.addEventListener("submit", function (event) {
   // Create Meme
   event.preventDefault();
+
   const newMemeObject = {
     description: event.target.description.value,
     meme: event.target.meme.value,
     likes: 0,
   };
 
-  renderOneMeme(newMemeObject);
+  // make a POST /posts request
+  fetch("http://localhost:3000/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newMemeObject),
+  })
+    .then((response) => response.json())
+    .then((actualNewMemeFromTheServer) => {
+      console.log("Success:", actualNewMemeFromTheServer);
+      // and slap the new post on the DOM
+      renderOneMeme(actualNewMemeFromTheServer);
+    });
+
+  console.log(newMemeObject);
+  // renderOneMeme(newMemeObject);
+
   event.target.reset();
 });
 
 /******************* Initial Render  ******************/
-renderAllListings(postsArray);
+
+// Do (GET /posts) Fetch Request...
+fetch("http://localhost:3000/posts")
+  .then((response) => response.json())
+  .then((postsArray) => {
+    console.log(postsArray);
+    // And Slap (our array of posts) On the DOM
+    renderAllListings(postsArray);
+  });
