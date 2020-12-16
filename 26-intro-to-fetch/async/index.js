303153624ac1b49a9f5c0a342e95180cd6701410
ihollander <ianholla@gmail.com>
2020-12-16 10:35:39 -0500

    After fetch

--- 26-intro-to-fetch/async/index.js
+++ 26-intro-to-fetch/async/index.js
@@ -1,10 +1,55 @@
-function sleep(time) {
-  const start = new Date();
-  while (new Date() - start < time) {
-    // do nothing && block the main thread 😈
-  }
+// console.log("BEFORE fetch");
+
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+const nameH3 = document.querySelector("h3");
+
+button.addEventListener("click", (event) => {
+  console.log(event.target);
+
+  const id = input.value;
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      renderPokemon(data);
+    });
+});
+
+function renderPokemon(pokemon) {
+  nameH3.textContent = "Base XP: " + pokemon.base_experience;
 }
 
-console.log("BRB");
-sleep(5000); // blocking
-console.log("OK i'm back!");
+// fetch("https://pokeapi.co/api/v2/pokemon/2")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log("INSIDE fetch");
+//     console.log(data);
+//   });
+
+// console.log("AFTER fetch");
+
+// sleep(5); /// blocking code!
+
+// // non blocking!
+// console.log("BEFORE setTimeout");
+
+// // asynchronous!
+// setTimeout(() => {
+//   console.log("INSIDE setTimeout");
+// }, 0);
+
+// console.log("AFTER setTimeout");
+// console.log("AFTER setTimeout");
+// console.log("AFTER setTimeout");
+// console.log("AFTER setTimeout");
+// console.log("AFTER setTimeout");
+// console.log("AFTER setTimeout");
+// console.log("AFTER setTimeout");
+// console.log("AFTER setTimeout");
+// console.log("AFTER setTimeout");
+// console.log("AFTER setTimeout");
+// console.log("AFTER setTimeout");
+// console.log("AFTER setTimeout");
+// console.log("AFTER setTimeout");
+// console.log("AFTER setTimeout");
--- 26-intro-to-fetch/async/sync.rb
+++ 26-intro-to-fetch/async/sync.rb
@@ -1,15 +1,32 @@
-# require 'rest-client'
-# require 'json'
-# require 'pry'
 
-puts "Let's get going"
-puts "But first, sleepytime"
-sleep 5
-puts "Thanks for that nice rest"
 
-# puts "Getting data from the API"
-# response = RestClient.get("https://pokeapi.co/api/v2/pokemon/2")
+# puts("Let's get going")
+# puts("But first, sleepytime")
+# sleep(5)
+# puts("Thanks for that nice rest")
 
-# puts "Welcome to PokeWorld!!!!"
-# pokemon = JSON.parse(response)
-# puts "Your pokemon is #{pokemon["name"]}!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+require 'rest-client'
+require 'json'
+require 'pry'
+
+puts "Getting data from the API"
+response = RestClient.get("https://pokeapi.co/api/v2/pokemon/2")
+pokemon = JSON.parse(response)
+puts "Your pokemon is #{pokemon["name"]}!"
