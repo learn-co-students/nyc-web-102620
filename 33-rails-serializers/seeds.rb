36335e34915149c588870e8bdb3a2e902a7b2708
Eric Kim <hyeokjungkim@gmail.com>
2020-12-24 13:02:31 -0500

    adds lecture

---
+++ 33-rails-serializers/seeds.rb
@@ -0,0 +1,13 @@
+eric = Teacher.create(name: "Eric", subject: "Math")
+ian = Teacher.create(name: "ian", subject: "Chemistry")
+
+geo = Classroom.create(name: "Geometry", teacher: eric)
+numbers = Classroom.create(name: "Numbers", teacher: eric)
+
+chem = Classroom.create(name: "Nuclear Chem 500", teacher: ian)
+
+Assignment.create(name: "Pythagorean Theorem", classroom: geo)
+Assignment.create(name: "Counting 101", classroom: numbers)
+
+Assignment.create(name: "Fission", classroom: chem)
+Assignment.create(name: "Fusion", classroom: chem)
