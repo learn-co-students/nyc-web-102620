e0cd9d7dd820ae3e55d17577e05503d377b72ba1
ihollander <ianholla@gmail.com>
2021-01-11 08:58:25 -0500

    before components and props

---
+++ 37-components-and-props/data.js
@@ -0,0 +1,34 @@
+const projects = [
+  {
+    id: 1,
+    name: "Reciplease",
+    about: "A recipe tracking app",
+    primaryTechnology: "Rails",
+    link: "https://www.epicurious.com/",
+    image: "https://via.placeholder.com/300",
+  },
+  {
+    id: 2,
+    name: "Kibbles N Bitz",
+    about: "Tinder for dogs",
+    primaryTechnology: "React",
+    link: null,
+    image: "https://via.placeholder.com/300",
+  },
+  {
+    id: 3,
+    name: "Alienwares",
+    about: "Etsy for aliens",
+    primaryTechnology: "React",
+    link: "https://www.etsy.com/",
+    image: "https://via.placeholder.com/300",
+  },
+  {
+    id: 4,
+    name: "Zookeepr",
+    about: "Donate to endangered animals",
+    primaryTechnology: "Javascript",
+    link: null,
+    image: "https://via.placeholder.com/300",
+  },
+];
